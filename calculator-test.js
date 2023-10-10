73b6849c873949f16e48fa2c18ae73955344617d
Jake <jodonnell1313@gmail.com>
2023-10-10 14:33:39 -0700

    submission of loan calculator and tests

---
+++ calculator-test.js
@@ -0,0 +1,13 @@
+
+it('should calculate the monthly rate correctly', function () {
+  const values = { amount: 10000, years: 5, rate: 5};
+  expect(calculateMonthlyPayment(values)).toEqual('188.71');
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  const values = { amount: 7948.61, years: 5, rate: 5};
+  expect(calculateMonthlyPayment(values)).toEqual('150.00');
+});
+
+
